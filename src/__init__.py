917cfa01e62efd6ece13226e74f1859257141a8e
deba-iitbh <debarghyad@iitbhilai.ac.in>
2024-01-31 23:25:14 +0530

    Added CORS

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
-from src.config.config import Config
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv, find_dotenv
 
+from src.config.config import Config
 from src.routes import api
 from src.constants import Base, engine
 
 # loading environment variables
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
+
 app = FastAPI(root_path="/api")
+origins = [
+    "http://localhost",
+    "http://localhost:5000",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # calling the dev configuration
 config = Config().dev_config
